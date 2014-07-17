78560a8025abaf1093520b25a9e65b8939acb587
Keri Clowes <kerilyn.clowes@gmail.com>
2014-07-17 16:09:00 -0600

    Use inject instead to be more cool

--- lib/monthly_balance.rb
+++ lib/monthly_balance.rb
@@ -5,17 +5,12 @@ class MonthlyBalance
 
   def initialize(directory)
     @directory = directory
-
   end
 
   def for(month, year)
-    balance = 0
     entries = CheckingAccount.new(@directory).entries_for(month, year) +
-    CreditCardAccount.new(@directory).entries_for(month, year)
-    entries.each do |entry|
-      balance += entry.dollar_amount
-    end
-    balance.round(2)
+      CreditCardAccount.new(@directory).entries_for(month, year)
+    entries.inject(0) { |acc, entry| acc + entry.dollar_amount }.round(2)
   end
 
 end
